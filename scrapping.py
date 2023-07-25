c2073efcc14aed68fa88f72d9e9c0eff88ad570f
1dia100mijar <andrephelipesant@gmail.com>
2023-07-25 12:55:58 +0100

    missing the support for more than 12 posts

--- scrapping.py
+++ scrapping.py
@@ -1,9 +1,12 @@
 import requests
 import time
+import re
+import csv
 from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
+import os as system
 
-def scappe(link):
+def scrape(link):
     driver = webdriver.Firefox()
     driver.get(link)
     time.sleep(10)
@@ -12,53 +15,60 @@ def scappe(link):
     article_text = str(article)
     mainPagePostsElem = article_text.split("<a")
     driver.close()
+    results = []
     for post in mainPagePostsElem:
-        if(post.__contains__("href")):
-            post = post.split("href=\"")[1]
-            post = post.split("\"")[0]
-            postLink = f"https://www.instagram.com{post}"
-            getPostInfo(postLink)
-            print("\n\n\n--------------------------------------------------\n--------------------------------------------------\n\n\n")
+        if(post.__contains__("href")):       
+            postLink = post.split("href=\"")[1]
+            postLink = postLink.split("\"")[0]
+            postLink = f"https://www.instagram.com{postLink}"
+            print(postLink)
+            ##Get the post alt information 
+            try:
+                altInformation = post.split("img alt=\"")[1]
+                altInformation = altInformation.split("\" class=\"")[0]
+            except:
+                altInformation = ''
+            result = getPostInfo(postLink, altInformation)
+            results.append(result)
+    return results
 
-def getPostInfo(postLink):
-    print(postLink)
+def getPostInfo(postLink, altInformation = ''):
     page = requests.get(postLink)
     soup = BeautifulSoup(page.content, 'html.parser')
     soup = str(soup)
-    likes = getPostLikes(soup)
     isVideoFlag = 0
     if(soup.__contains__("content=\"video\"")):
         isVideoFlag=1
-
+        Content_type = "video"
+    else:
+        Content_type = "image"
+    likes = getPostLikes(soup)
     comments = getPostComments(soup) 
     description = getDescription(soup)
     hashtags = getHashtags(description)
     time = getPostDate(postLink, isVideoFlag)
-    # print(likes)
-    # print(description)
-    # print(hashtags)
-    # print(comments)
-    # print(time)
-    print(f'Post: {postLink}\nLikes: {likes}\nComments: {comments}\nDescription: {description}\nHashtags: {hashtags}\nTime: {time[0]}\n')
-    if(isVideoFlag):
-        print(f'Views: {time[1]}\n')
-
+    return [postLink, time[0], time[1], Content_type, time[2], likes, comments, description, hashtags, altInformation]
+     
 def getPostLikes(information):
     likes = information.split("likes,")[0]
-    return likes.split("'")[-1]
-
-def getPostViewes(information):
-    views = information.split("views")[0]
-    return views.split(",")[-1].strip()
+    separator = "'"
+    likes = likes.split(separator)[-1]
+    if(re.search('[a-zA-Z]', likes)):
+        separator = "\""
+        likes = likes.split(separator)[-1]
+    return int(likes.strip())
 
 def getPostComments(information):
     comments = information.split("comments")[0]
-    return comments.split(",")[-1].strip()
+    return int(comments.split(",")[-1].strip())
     
 
 def getDescription(information):
     description = information.split("<title>")[1]
-    return description.split("\"")[1]
+    description = description.split("\"")[1]
+    if description == "HasteSupportData":
+        description = ""
+    return description
 
 def getHashtags(description):
     rawData = description.split("#")
@@ -84,7 +94,7 @@ def getPostDate(link, videoFlag = 0):
         views = soup.find('span', class_='_aauw')
         views = str(views)
         views = views.split("<span>")[1]
-        views = views.split("</span>")[0]
+        views = int(views.split("</span>")[0].strip())
 
     timev = soup.find('time')
     timev = str(timev)
@@ -92,12 +102,32 @@ def getPostDate(link, videoFlag = 0):
     timev = timev.split("\"")[0]
     timev = timev.replace("T", " ")
     timev = timev.replace("Z", "")
+    timev = timev.split(" ")
     driver.close()
-    return [timev, views]
+    return [timev[0], timev[1], views]
 
+def initializeCSV(fileName, results):
+    with open(fileName, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["post", "day", "time", "content_type", "views", "likes", "comments", "description", "hashtags", "alt_information"])
+        for result in results:
+            escaped_result = [item.replace("\n", r"\n") if isinstance(item, str) else item for item in result]
+            writer.writerow(escaped_result)
+
+def createFolder(folderName):
+    distFolder = f"./{folderName}"
+    if not system.path.exists(distFolder):
+        system.makedirs(distFolder)
+    return distFolder
 
 def main():
-    scappe("https://www.instagram.com/sportingcp/")
+    fileName = "instagram"
+    folder = createFolder("Results")      
+    fileName = f"{folder}/{fileName}.csv"
+    print(fileName)
+    results = scrape("Enter the link of the instagram page")
+    initializeCSV(fileName, results)
+
 
 
 if __name__ == '__main__':
